dc04b64e042f1d25c9828b6f7c76583b35b672dc
Dorjee Thinlay <dthinley@yahoo.com>
2018-05-15 13:02:44 -0700

    Add files via upload

--- number_game/js/main.js
+++ number_game/js/main.js
@@ -1,1 +1,27 @@
 
+//variables
+var secretNo =  Math.floor((Math.random() * 5) + 1);
+var input = document.getElementById('input');
+var btn = document.getElementById('btn');
+var guesses =document.querySelector('.guessedRight');
+var lowOrHi = document.querySelector('.lowOrHi');
+
+
+
+//Event Listener and function
+btn.addEventListener('click', function(){
+	var input = document.getElementById('input').value;
+	if(input==secretNo){
+		guesses.textContent = 'Congratulations! You got it right!';
+		lowOrHi.textContent = '';
+	}else if(input>secretNo){
+		lowOrHi.textContent = 'Sorry! Too high. Try again..';
+	} else{(input<secretNo)
+		lowOrHi.textContent = 'Sorry! Too low. Try again..';
+	}
+
+
+});
+
+
+
