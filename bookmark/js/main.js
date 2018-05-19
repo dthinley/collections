7ba695e37e92059a4f7b04977abd7ea42f2218f1
Dorjee Thinlay <dthinley@yahoo.com>
2018-05-19 00:33:14 -0700

    Add files via upload

--- bookmark/js/main.js
+++ bookmark/js/main.js
@@ -1,1 +1,33 @@
+// variables
+
+var btnSubmit = document.getElementById("add");
+var list = document.getElementById("list");
+
+
+
+//Event Listener
+document.getElementById("form").addEventListener("submit", addBookmark);
+	
+
+
+
+//Functions
+function addBookmark(e){
+	e.preventDefault();
+	var siteName = document.getElementById('sitesName').value;
+	var siteUrl =document.getElementById('url').value;
+	
+	var addBookmark ={
+		name: siteName,
+		url: siteUrl
+	}
+	
+	addBookmark.content =list;
+	console.log(addBookmark)
+	
+
+	
+	
+
+}
 
