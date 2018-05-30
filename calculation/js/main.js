e38e29c35d294645ede517b02522f28a211a2ee2
Dorjee Thinlay <dthinley@yahoo.com>
2018-05-29 22:48:05 -0700

    Add files via upload

--- calculation/js/main.js
+++ calculation/js/main.js
@@ -1,1 +1,28 @@
+var firstNo	= document.getElementById("firstNo");
+var secondNo = document.getElementById("secondNo");
+var addbtn = document.getElementById("add");
+var multiplybtn = document.getElementById("multiply");
 
+//Event Listener
+
+addbtn.addEventListener("click", addition);
+multiplybtn.addEventListener("click", multiply);
+	
+
+function addition(e){
+	e.preventDefault();
+	var firstNo	= document.getElementById("firstNo").value;
+	var secondNo = document.getElementById("secondNo").value;
+	var result = +firstNo + +secondNo;
+	document.getElementById("result").innerHTML =result;
+	document.getElementById("result").className ="result";
+}
+
+function multiply(e){
+	e.preventDefault();
+	var firstNo	= document.getElementById("firstNo").value;
+	var secondNo = document.getElementById("secondNo").value;
+	var result = +firstNo * +secondNo;
+	document.getElementById("result").innerHTML =result;
+	document.getElementById("result").className ="result"
+}
